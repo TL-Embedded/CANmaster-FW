d8b7c9357a2fc37288b729e2e299ed233078dee7
Tim Lamborn <tlamborn@gmail.com>
2022-05-08 15:32:49 +1200

    Board runup

--- Core/Board.h
+++ Core/Board.h
@@ -2,61 +2,35 @@
 #define BOARD_H
 
 #define STM32F0
-//#define STM32F0
 
 // Core config
 //#define CORE_USE_TICK_IRQ
 
 // CLK config
-//#define CLK_USE_HSE
-//#define CLK_USE_LSE
-//#define CLK_LSE_BYPASS
-//#define CLK_LSE_FREQ		32768
-
-// RTC config
-//#define RTC_USE_IRQS
-
-// US config
-//#define US_TIM			TIM_22
-//#define US_RES			1
-
-// ADC config
-//#define ADC_VREF	        3300
-
-// GPIO config
-//#define GPIO_USE_IRQS
-//#define GPIO_IRQ0_ENABLE
-
-// TIM config
-//#define TIM_USE_IRQS
-//#define TIM2_ENABLE
-
-// UART config
-//#define UART1_GPIO		GPIOA
-//#define UART1_PINS		(GPIO_PIN_9 | GPIO_PIN_10)
-//#define UART1_AF		    GPIO_AF4_USART1
-//#define UART_BFR_SIZE     128
-
-// SPI config
-//#define SPI1_GPIO		    GPIOB
-//#define SPI1_PINS		    (GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5)
-//#define SPI1_AF			GPIO_AF0_SPI1
-
-// I2C config
-//#define I2C1_GPIO			GPIOB
-//#define I2C1_PINS			(GPIO_PIN_6 | GPIO_PIN_7)
-//#define I2C1_AF			GPIO_AF1_I2C1
-//#define I2C_USE_FASTMODEPLUS
+#define CLK_USE_HSE
+#define CLK_HSE_FREQ		8000000
+#define CLK_SYSCLK_FREQ		32000000
 
 // CAN config
-//#define CAN_GPIO			GPIOB
-//#define CAN_PINS			(GPIO_PIN_8 | GPIO_PIN_9)
-//#define CAN_AF			GPIO_AF4_CAN
-//#define CAN_DUAL_FIFO
+#define CAN_GPIO			GPIOB
+#define CAN_PINS			(GPIO_PIN_8 | GPIO_PIN_9)
+#define CAN_AF				GPIO_AF4_CAN
 
 // USB config
-//#define USB_ENABLE
-//#define USB_CLASS_CDC
-//#define USB_CDC_BFR_SIZE	512
+#define USB_ENABLE
+#define USB_CLASS_CDC
+#define USB_CDC_BFR_SIZE	512
+
+
+// GPIO config
+#define CAN_MODE_GPIO		GPIOC
+#define CAN_MODE_PIN		GPIO_PIN_13
+#define CAN_TERM_GPIO		GPIOA
+#define CAN_TERM_PIN		GPIO_PIN_3
+
+#define LED_TX_GPIO			GPIOA
+#define LED_TX_PIN			GPIO_PIN_4
+#define LED_RX_GPIO			GPIOA
+#define LED_RX_PIN			GPIO_PIN_15
 
 #endif /* BOARD_H */
--- Core/main.c
+++ Core/main.c
@@ -1,16 +1,25 @@
 
 #include "Core.h"
+#include "GPIO.h"
 
-
+#include "USB.h"
 
 
 int main(void)
 {
 	CORE_Init();
 
+	USB_Init();
+
+	GPIO_EnableOutput(LED_TX_GPIO, LED_TX_PIN, false);
+	GPIO_EnableOutput(LED_RX_GPIO, LED_RX_PIN, false);
+
 	while(1)
 	{
-		CORE_Idle();
+		CORE_Delay(1000);
+		USB_CDC_WriteStr("Tick\r\n");
+
+		//CORE_Idle();
 	}
 }
 
